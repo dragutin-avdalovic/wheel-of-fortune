efef19a56a49cf0b51608731e86fb84f277bbd9d
Dragutin <dragutin.avdalovic@walter.ba>
2019-07-01 16:55:00 +0200

    Leaderboard added

---
+++ dist/precache-manifest.cb4f3fe6bfbff17ffe2c10cdb361a547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e04c4508d4db96d789d6",
+    "url": "/css/app.f6e61016.css"
+  },
+  {
+    "revision": "e04c4508d4db96d789d6",
+    "url": "/js/app.2d61d964.js"
+  },
+  {
+    "revision": "596189085f444534b8eb",
+    "url": "/css/chunk-vendors.881f9b12.css"
+  },
+  {
+    "revision": "596189085f444534b8eb",
+    "url": "/js/chunk-vendors.fa30ba3e.js"
+  },
+  {
+    "revision": "9bd27519af5056767a30dcdc386e0720",
+    "url": "/img/spinner-marker.9bd27519.svg"
+  },
+  {
+    "revision": "6ecd595d0302ca38a5f467c7a88e6a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
